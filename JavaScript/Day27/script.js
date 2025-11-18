8f41417119a6f8181ec063552184e74f9e626c09
Darshan Takarkhede <100999462+NeoAnthem@users.noreply.github.com>
2025-11-19 01:07:14 +0530

    Add files via upload

--- JavaScript/Day27/script.js
+++ JavaScript/Day27/script.js
@@ -1,3 +1,6 @@
+// Import
+import Swal from "https://cdn.jsdelivr.net/npm/sweetalert2@11/+esm";
+
 // Elements
 const form = document.getElementById("regForm");
 const submitBtn = document.getElementById("submitBtn");
@@ -126,10 +129,10 @@ const validators = {
   email: (value) => /^\S+@\S+\.\S+$/.test(value),
   username: (value) => value.trim() !== "" && !/\s/.test(value),
   password: (value) =>
-    /^(?=.*[A-Z])(?=.*[!@#$%^&*+]])(?=.*\d)\S{8,}$/.test(value),
+    /^(?=.*[A-Z])(?=.*[!@#$%^&*+])(?=.*\d)\S{8,}$/.test(value),
   country: (value) => value.trim() !== "",
   city: (value) => value.trim() !== "",
-  phone: (value) => /^\d{7,15} $/.test(value),
+  phone: (value) => /^\d{7,15}$/.test(value),
   dob: (value) => {
     if (!value) {
       return false;
@@ -159,3 +162,195 @@ const messages = {
   dob: "Please select valid DOB",
   appointment: "Previous dates are not applicable",
 };
+
+// Utility functions -- Helper Functions
+// set Valid State / set Invalid State
+
+function setValidState(id) {
+  const element = fields[id];
+  // console.log(element);
+  const icon = icons[id];
+
+  element.classList.remove("invalid");
+  element.classList.add("valid");
+  if (icon) {
+    icon.className = "fa input-icon valid fa-regular fa-circle-check";
+  }
+  if (errors[id]) {
+    errors[id].textContent = "";
+  }
+}
+
+function setInValidState(id) {
+  const element = fields[id];
+  const icon = icons[id];
+
+  element.classList.remove("valid");
+  element.classList.add("invalid");
+
+  icon.className = "fa input-icon invalid fa-regular fa-circle-xmark";
+  if (touched[id]) {
+    errors[id].textContent = messages[id];
+  }
+}
+
+function clearState(id) {
+  const element = fields[id];
+  const icon = icons[id];
+
+  element.classList.remove("invalid", "valid");
+  if (icon) {
+    icon.className = "fa input-icon";
+  }
+  if (errors[id]) {
+    errors[id].textContent = "";
+  }
+}
+
+// Main Function Which will be called to check validations for each field
+function validateField(id, showImmediately = false) {
+  const element = fields[id];
+  const value = (element.value || "").trim();
+
+  // check if that field is touched or untouched
+  if (!touched[id] && !showImmediately) {
+    clearState(id);
+    return validators[id] ? validators[id](value) : true;
+  }
+
+  let validResponse = validators[id] ? validators[id](value) : true;
+
+  if (validResponse) {
+    setValidState(id);
+  } else {
+    setInValidState(id);
+  }
+  return validResponse;
+}
+
+// On Focus on any field mark that field as touched -> change false to true
+
+Object.keys(fields).forEach((formField) => {
+  fields[formField].addEventListener("focus", () => {
+    touched[formField] = true;
+  });
+});
+
+// Call validator when blur
+
+Object.keys(fields).forEach((formField) => {
+  // console.log(fields[formField]);
+  fields[formField].addEventListener("blur", () => {
+    validateField(formField, true);
+    updateSubmitButton();
+  });
+});
+
+// If everything is correct update submiut button
+function updateSubmitButton() {
+  const allIds = Object.keys(fields);
+  // console.log(allIds);
+  const allValid = allIds.every((formField) => {
+    return validateField(formField, false); // it won't check for untouched
+  });
+  submitBtn.disabled = !allValid;
+  submitBtn.classList.toggle("active", allValid);
+}
+
+// Logic for Age in years and months
+fields.dob.addEventListener("change", () => {
+  const dateValue = fields.dob.value;
+
+  if (!dateValue) {
+    fields.ageYears.value = "";
+    fields.ageMonths.value = "";
+  }
+
+  const dobDate = new Date(dateValue);
+  const now = new Date();
+  let years = now.getFullYear() - dobDate.getFullYear();
+  let months = now.getMonth() - dobDate.getMonth();
+
+  if (months < 0) {
+    years--;
+    months = months + 12;
+  }
+
+  fields.ageYears.value = `${years} years`;
+  fields.ageMonths.value = `${months} months`;
+  touched.dob = true;
+  validateField("dob", true);
+  updateSubmitButton();
+});
+
+// Toggle Password
+togglePasswordBtn.addEventListener("click", () => {
+  const pwd = fields.password;
+  const showing = pwd.type === "text";
+
+  pwd.type = showing ? "password" : "text";
+
+  // eye button to be changes
+
+  eyeIcon.classList.toggle("fa-eye");
+  eyeIcon.classList.toggle("fa-eye-slash");
+});
+
+// submit logic
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // mark all as touched and check for validations
+  Object.keys(touched).forEach((field) => {
+    touched[field] = true;
+  });
+  const allIds = Object.keys(fields);
+
+  const allValids = allIds.every((id) => validateField(id, true));
+  if (allValids) {
+    // backend call
+
+    // show alert with a message form submitted
+    Swal.fire({
+      title: "Done!",
+      text: "Data Saved Successfully🎉",
+      icon: "success",
+    });
+
+    form.reset();
+    // clear all UI
+    Object.keys(fields).forEach((field) => {
+      fields[field].classList.remove("valid", "invalid");
+      if (icons[field]) {
+        icons[field].className = "fa input-icon";
+      }
+      if (errors[field]) {
+        errors[field].textContent = "";
+      }
+      touched[field] = false;
+    });
+
+    // disabled country code and city
+    fields.countryCode.disabled = true;
+    fields.city.disabled = true;
+
+    submitBtn.disabled = true;
+    submitBtn.classList.remove("active");
+
+    // restore password to password and with eye icon
+    fields.password.type = "password";
+    eyeIcon.classList.add("fa-eye");
+    eyeIcon.classList.remove("fa-eye-slash");
+  } else {
+    allIds.forEach((id) => validateField(id, true));
+
+    // extra feature -> focus on first error form input
+    const firstInvalid = allIds.find((id) => !validateField(id, true));
+
+    if (firstInvalid) {
+      fields[firstInvalid].focus();
+    }
+  }
+});
+
+updateSubmitButton();
